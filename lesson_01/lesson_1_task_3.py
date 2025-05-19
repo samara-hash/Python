016204a9af2c363e0afb8b816b2d93491eef4bf0
unknown <mari.voronchikova@gmail.com>
2025-05-19 15:37:01 +0300

    first commit

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = input("Введите ваше имя: ")
 last_name = input("Введите вашу фамилию: ")
-print("Вас зовут: " + first_name + " " + last_name)
+first_name = input("Введите ваше имя: ")
+print("Вас зовут: " + last_name + " " + first_name)
